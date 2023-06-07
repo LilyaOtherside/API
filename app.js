eaa8ea8f5739b4abed6a8d382e6a7273dbe692e5
LilyaOtherside <34894214+LilyaOtherside@users.noreply.github.com>
2023-06-08 01:42:48 +0300

    Edit app.js

--- app.js
+++ app.js
@@ -1,95 +1,45 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const AWS = require('aws-sdk');
 
 const app = express();
 
 // parse application/json
 app.use(bodyParser.json());
 
-// Configure AWS credentials and region
-AWS.config.update({
-  accessKeyId: 'ASIAQDSB7LRV6DVIG4MO',
-  secretAccessKey: '/TKyXfhyda21YS8xXSXgz5ZOG+95/zkNJ/F7cu4y',
-  region: 'eu-west-2'
-});
-
-const dynamoDB = new AWS.DynamoDB();
+let consents = [];
 
 // Get consent by key
 app.get('/consent/:key', (req, res) => {
-  const params = {
-    TableName: 'your_table_name',
-    Key: {
-      key: { S: req.params.key }
-    }
-  };
-
-  dynamoDB.getItem(params, (err, data) => {
-    if (err) {
-      console.error('Error retrieving consent:', err);
-      res.status(500).send('Internal Server Error');
-    } else {
-      if (data.Item) {
-        res.send({ consent: data.Item.text.S });
-      } else {
-        res.status(404).send('Consent not found');
-      }
-    }
-  });
+    const consent = consents.find(c => c.key === req.params.key);
+    if (!consent) return res.status(404).send('Consent not found');
+    res.send({consent: consent.text});
 });
 
 // Create a new consent
 app.post('/consent', (req, res) => {
-  const params = {
-    TableName: 'your_table_name',
-    Item: {
-      key: { S: req.body.key },
-      text: { S: req.body.text }
-    }
-  };
-
-  dynamoDB.putItem(params, (err, data) => {
-    if (err) {
-      console.error('Error creating consent:', err);
-      res.status(500).send('Internal Server Error');
-    } else {
-      res.status(201).send(params.Item);
-    }
-  });
+    const consent = {
+        key: req.body.key,
+        text: req.body.text
+    };
+    consents.push(consent);
+    res.status(201).send(consent);
 });
 
 // Update a consent
 app.put('/consent/:key', (req, res) => {
-  const params = {
-    TableName: 'helpful-gold-newtCyclicDB',
-    Key: {
-      key: { S: req.params.key }
-    },
-    UpdateExpression: 'SET #text = :text',
-    ExpressionAttributeNames: { '#text': 'text' },
-    ExpressionAttributeValues: { ':text': { S: req.body.text } }
-  };
+    const consent = consents.find(c => c.key === req.params.key);
+    if (!consent) return res.status(404).send('The consent with the given key was not found.');
 
-  dynamoDB.updateItem(params, (err, data) => {
-    if (err) {
-      console.error('Error updating consent:', err);
-      res.status(500).send('Internal Server Error');
-    } else {
-      if (data.Attributes) {
-        res.send(data.Attributes);
-      } else {
-        res.status(404).send('Consent not found');
-      }
-    }
-  });
+    consent.text = req.body.text;
+
+    res.send(consent);
 });
 
 let port = process.env.PORT;
-if (port == null || port == '') {
+if (port == null || port == "") {
   port = 8000;
 }
 
 app.listen(port, () => {
-  console.log('Server running');
+ console.log('Server running');
 });
